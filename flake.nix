faf6936f10b097a482d1a8f2e5afa3a8889635ed
nekolyanich <gmail@nekolyanich.com>
2022-06-16 15:02:22 +0300

    flake fixes

--- flake.nix
+++ flake.nix
@@ -5,41 +5,39 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.poetry2nix.url = "github:nix-community/poetry2nix";
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
+    overlay = nixpkgs.lib.composeManyExtensions [
+      poetry2nix.overlay
+      (final: prev: {
+        magnit-calc = prev.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          python = prev.python310;
+        };
+        magnit-calc-env = prev.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          python = prev.python310;
+        };
+      })
+    ];
+  } // (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
+    in
     {
-      # Nixpkgs overlay providing the application
-      overlay = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: prev: {
-          # The application
-          magnit-calc = prev.poetry2nix.mkPoetryApplication {
-            projectDir = ./.;
-            python = prev.python310;
-          };
-          magnit-calc-env = prev.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-            python = prev.python310;
-          };
-        })
-      ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-        };
-      in
-      {
-        apps = {
-          magnit-calc = pkgs.magnit-calc;
-        };
-        defaultApp = pkgs.magnit-calc;
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.python310Packages.poetry
-            pkgs.magnit-calc-env
-            pkgs.redis
-          ];
-        };
-      }));
+      apps = {
+        magnit-calc = pkgs.magnit-calc;
+      };
+      defaultApp = pkgs.magnit-calc;
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.python310Packages.poetry
+          pkgs.magnit-calc-env
+          pkgs.redis
+        ];
+      };
+    }
+  ));
 }
